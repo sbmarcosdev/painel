8cc4a2255fc2523ebc07f1f82a4788b40e60c5a0
sbmarcosdev <marcos.silvabarros@gmail.com>
2021-05-11 20:35:15 -0300

    app config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,14 +35,14 @@
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     <img src="{{ asset('img/logo_bti.png') }}" width="82px" heigth="54">
                     @guest Painel de Integrações @else {{ App\Empresa::find(Auth::user()->empresa_id)->nome ?? '' }} @endguest
-                    {{-- {{ config('app.name', 'Laravel') }} --}}
+
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
