736b092d89e85e7f1686224154eba6e3a87c15b0
sbmarcosdev <marcos.silvabarros@gmail.com>
2021-06-03 21:17:05 -0300

    getEmpre

--- app/Http/Controllers/TabelaController.php
+++ app/Http/Controllers/TabelaController.php
@@ -20,7 +20,7 @@ class TabelaController extends Controller
     public function index()
     {
         if (session()->get('isAdmin')){
-            $tabelas = Tabela::all()->orderBy('empresa_id');
+            $tabelas = Tabela::orderBy('empresa_id')->get();
         }else{
             $tabelas = Tabela::where('empresa_id', Auth::user()->empresa_id )->get();
         }
